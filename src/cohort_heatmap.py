9877cfce7de8be22b850c874fa17e948067377a7
Tuba Nur Demir <tubanurdemir4@gmail.com>
2026-05-23 12:01:12 +0300

    Update cohort_heatmap.py

--- src/cohort_heatmap.py
+++ src/cohort_heatmap.py
@@ -3,18 +3,27 @@ Cohort Analysis & Retention Heatmap (Percentage Based)
 This script connects to the PostgreSQL database, calculates customer retention
 using a SQL CTE, converts raw numbers to retention rates (%), and generates a heatmap.
 """
-
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sqlalchemy import create_engine
-
+from dotenv import load_dotenv
 # -------------------------------------------------------------------
 # 1. DATABASE CONNECTION (ETL - Extract)
 # -------------------------------------------------------------------
 # Connects to the local PostgreSQL database using SQLAlchemy
-engine = create_engine('postgresql://postgres:190719@localhost:5432/ecommerce_db')
+load_dotenv()
 
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DB_URL)
 # -------------------------------------------------------------------
 # 2. SQL QUERY (Data Processing)
 # -------------------------------------------------------------------
